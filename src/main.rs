8e5ba4ca9ca7827a41c59af72732d3272a56935a
Sunjay Varma <varma.sunjay@gmail.com>
2020-05-09 14:04:21 -0400

    Adding a crate to show a file open dialog

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use nfd2::Response;
+
+fn main() -> anyhow::Result<()> {
+    let path = match nfd2::open_file_dialog(Some("tmx"), None)? {
+        Response::Okay(file_path) => println!("File path = {:?}", file_path),
+        Response::OkayMultiple(files) => println!("Files {:?}", files),
+        Response::Cancel => println!("User canceled"),
+    };
+
+    Ok(())
 }
