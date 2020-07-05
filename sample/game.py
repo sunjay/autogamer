c9f39ecec3d25cfdefaced46f099508d0eeb2cc4
Sunjay Varma <varma.sunjay@gmail.com>
2020-07-05 12:41:41 -0600

    Specifying window width/height in Game constructor

--- sample/game.py
+++ sample/game.py
@@ -6,7 +6,7 @@ from pyautogamer.ui import *
 from screens.main_menu import MainMenu
 
 def main():
-    game = Game()
+    game = Game(window_width=800, window_height=600)
 
     main_menu = MainMenu(game)
     game.set_screen(main_menu)
