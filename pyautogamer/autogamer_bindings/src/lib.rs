615a302f254f63ca2c2ff730da939af15a8eb865
Sunjay Varma <varma.sunjay@gmail.com>
2020-07-05 19:12:52 -0600

    Allowing the user to configure the window width and window height on game using the constructor

--- pyautogamer/autogamer_bindings/src/lib.rs
+++ pyautogamer/autogamer_bindings/src/lib.rs
@@ -25,22 +25,36 @@ pub fn ui(_py: Python, pymod: &PyModule) -> PyResult<()> {
 #[pyclass(gc)]
 #[derive(Debug)]
 pub struct Game {
+    window_width: u32,
+    window_height: u32,
     current_screen: Option<Py<Screen>>
 }
 
 #[pyproto]
 impl PyGCProtocol for Game {
     fn __traverse__(&self, visit: PyVisit) -> Result<(), PyTraverseError> {
-        let Self {current_screen} = self;
+        let Self {
+            window_width: _,
+            window_height: _,
+            current_screen,
+        } = self;
+
         visit.call(current_screen)?;
+
         Ok(())
     }
 
     fn __clear__(&mut self) {
-        let Self {current_screen} = self;
+        let Self {
+            window_width: _,
+            window_height: _,
+            current_screen,
+        } = self;
+
         // Release reference, this decrements the ref counter
         let gil = GILGuard::acquire();
         let py = gil.python();
+
         if let Some(current_screen) = current_screen.take() {
             py.release(&current_screen);
         }
@@ -50,8 +64,18 @@ impl PyGCProtocol for Game {
 #[pymethods]
 impl Game {
     #[new]
-    pub fn new() -> Self {
+    #[args(
+        "*",
+        window_width = 800,
+        window_height = 600,
+    )]
+    pub fn new(
+        window_width: u32,
+        window_height: u32,
+    ) -> Self {
         Self {
+            window_width,
+            window_height,
             current_screen: None,
         }
     }
